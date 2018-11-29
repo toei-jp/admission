1da0dd3260ef16b2baeec78bf9611ffdf2cf1ac5
Akito Hataguchi <hataguchi@motionpicture.jp>
2018-11-29 15:08:33 +0900

    TOEI-51 本番用に調整

--- src/server/app/routes/router.ts
+++ src/server/app/routes/router.ts
@@ -18,7 +18,6 @@ export default (app: express.Application) => {
     app.get('/signIn', authorize.signOutRedirect);
 
     app.get('*', (_req, res, _next) => {
-        const fileName = (process.env.NODE_ENV === 'production') ? 'production.html' : 'index.html';
-        res.sendFile(path.resolve(`${__dirname}/../../../client/${process.env.NODE_ENV}/${fileName}`));
+        res.sendFile(path.resolve(`${__dirname}/../../../client/${process.env.NODE_ENV}/index.html`));
     });
 };
