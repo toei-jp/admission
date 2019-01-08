7131022ee17ec1af5e1d40384d362911b3987cc9
Akito Hataguchi <hataguchi@motionpicture.jp>
2019-01-08 15:18:00 +0900

    TOEI-121 エラーハンドリング修正

--- dist/server/app/routes/router.js
+++ dist/server/app/routes/router.js
@@ -11,7 +11,19 @@ exports.default = (app) => {
     app.use('/api/authorize', authorize_1.default);
     app.get('/signIn', authorize.signInRedirect);
     app.get('/signIn', authorize.signOutRedirect);
-    app.get('*', (_req, res, _next) => {
+    app.get('*', (req, res, _next) => {
+        if (req.xhr) {
+            res.status(httpStatus.NOT_FOUND).json('NOT FOUND');
+            return;
+        }
         res.sendFile(path.resolve(`${__dirname}/../../../client/${process.env.NODE_ENV}/index.html`));
+    });
+    app.post('*', (req, res, _next) => {
+        res.status(httpStatus.NOT_FOUND);
+        if (req.xhr) {
+            res.json('NOT FOUND');
+            return;
+        }
+        res.redirect('/#/error');
     });
 };
