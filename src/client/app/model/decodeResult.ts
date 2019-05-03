a361a966378712ef11d5ade5834107de9d9d1c68
Akito Hataguchi <hataguchi@motionpicture.jp>
2019-05-03 14:39:20 +0900

    TOEI-274 予約データを最適化

--- src/client/app/model/decodeResult.ts
+++ src/client/app/model/decodeResult.ts
@@ -1,7 +1,12 @@
 import { factory } from '@cinerino/api-javascript-client';
 
-type IReservation = factory.chevre.reservation.event.IReservation<factory.chevre.event.screeningEvent.IEvent>;
+export interface IReservation {
+    id: string;
+    reservationStatus: factory.chevre.reservationStatusType;
+    reservedTicket: factory.chevre.reservation.ITicket<factory.chevre.reservationType.EventReservation>;
+}
 
-export interface IDecodeResult extends factory.ownershipInfo.IOwnershipInfo<IReservation> {
+// tslint:disable-next-line:max-line-length
+export interface IDecodeResult extends factory.ownershipInfo.IOwnershipInfo<factory.chevre.reservation.IReservation<factory.chevre.reservationType.EventReservation>> {
     iat: number;
 }
--- src/client/environments/environment.ts
+++ src/client/environments/environment.ts
@@ -4,9 +4,10 @@
  */
 export const environment = {
     production: true,
-
+    PROJECT_ID: 'toei-production',
     ENV: 'production',
-
+    STORAGE_NAME: 'TOEI-ADMISSION-STATE',
+    STORAGE_TYPE: 'localStorage',
     ANALYTICS_ID: ''
 
 };
